e5885b8b1ceba37967b4874922874d0fdeefffa4
anton <skrebnev.anton@gmail.com>
2019-05-04 17:43:16 +0700

    debug random_n

--- src/core.c
+++ src/core.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
 #include <math.h>
-#include <time.h>
-#include <sys/time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
 int random_n(int kol)
 {
     srand(time(NULL));
-    int n,ind,numb = 0,m;
-    for (int i=1;i<=kol;i++) {
-        ind=1;
+    int n, ind, numb = 0, m;
+    for (int i = 1; i <= kol; i++) {
         do {
-            n=rand() % 10;
-            for (int b=1; b<i;b++) {
-                m=numb/(pow(10,b-1));
-//              printf("--%d--%d--\n",m,numb);
-                if (n == (m%10))
-                    ind=0;
+            ind = 1;
+            n = rand() % 10;
+            for (int b = 1; b < i; b++) {
+                m = numb / (pow(10, b - 1));
+                if (n == (m % 10))
+                    ind = 0;
             }
-        } while(ind==0);
-        numb=numb + (n*pow(10,i-1));
-        printf("%d %d\n",numb,n);
+        } while (ind == 0);
+        numb = numb + (n * pow(10, i - 1));
     }
     return numb;
 }
 
 int main()
 {
-int kol;
-scanf("%d",&kol);
-printf("\n %d",random_n(kol+3));
 }
