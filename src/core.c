16a5db76968b7d912e40e5edc8eb092922f6875b
Ant2000s <skrebnev.anton@gmail.com>
2019-05-28 18:03:49 +0700

    bugfix random in core.c

--- src/core.c
+++ src/core.c
@@ -1,9 +1,11 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/time.h>
+#include <time.h>
 int random_n(int kol)
 {
+    srand(time(NULL));
     int n, ind, numb = 0, m;
     for (int i = 1; i <= kol; i++) {
         do {
