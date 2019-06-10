fef1eb558531f62813779a65222fc1b9c05177b0
Ant2000s <skrebnev.anton@gmail.com>
2019-06-10 23:19:39 +0700

    add mode for two players

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main()
     p = fopen("../src/rules.txt", "rt");
     while (1) {
 	print_menu();
-        h = choose_point();
+        h = choose_point(4);
         if (h == 2) {
             p = fopen("../src/rules.txt", "rt");
             print_txt(p);
@@ -20,16 +20,37 @@ int main()
             printf("\t2. Medium\n");
             printf("\t3. Difficult\n");
             printf("Input value: ");
-            kol = choose_point();
+            kol = choose_point(3);
             kol = kol + 3;
             num = random_n(kol);
             do {
+                printf("Input your number: ");
                 i = scans(kol);
                 bulls = how_much_bulls(i, num, kol);
                 cows = how_much_cows(i, num, kol);
                 printf("bulls: %d\n cows: %d\n", bulls, cows);
             } while (bulls != kol);
             printf("Congratulations! You won!");
+        }
+        if (h==4) {
+            printf("Choose the difficultness:\n");
+            printf("\t1. Easy\n");
+            printf("\t2. Medium\n");
+            printf("\t3. Difficult\n");
+            printf("Input value: ");
+            kol = choose_point(3);
+            kol = kol + 3;
+            int H = 1;
+            num = random_n(kol);
+            do {
+                H++;
+                printf("   Player %d \ninput your number: ",(H%2)+1);
+                i = scans(kol);
+                bulls = how_much_bulls(i, num, kol);
+                cows = how_much_cows(i, num, kol);
+                printf("bulls: %d\n cows: %d\n", bulls, cows);
+            } while (bulls != kol);
+            printf("Congratulations! Player %d won!",(H%2)+1);
         }
         if (h == 3) {
             return 0;
