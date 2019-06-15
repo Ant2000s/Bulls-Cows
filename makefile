fcdda484d7d8953d51e75eba54cbace806c22a85
wanderer <github.com/GreyWayfarer>
2019-06-16 00:02:56 +0700

    Add BC counter test

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
-all: bin/main bin/main-test
-bin/main: build/main.o build/core.o
-	gcc -Wall -Werror build/main.o build/core.o -o bin/main -lm
+all: bin/Bulls-Cows bin/Bulls-Cows-test
+bin/Bulls-Cows: build/main.o build/core.o
+	gcc -Wall -Werror build/main.o build/core.o -o bin/Bulls-Cows -lm
 
 build/main.o: src/main.c
 	gcc -Wall -Werror -c src/main.c -o build/main.o
@@ -8,8 +8,8 @@ build/main.o: src/main.c
 build/core.o: src/core.c
 	gcc -Wall -Werror -c src/core.c -o build/core.o -lm
 
-bin/main-test: build/test/main.o build/core.o build/test/input.o
-	gcc -Wall -Werror build/test/main.o build/test/input.o build/core.o -o bin/main-test -lm
+bin/Bulls-Cows-test: build/test/main.o build/core.o build/test/input.o
+	gcc -Wall -Werror build/test/main.o build/test/input.o build/core.o -o bin/Bulls-Cows-test -lm
 
 build/test/main.o: test/main.c
 	gcc -I thirdparty -Wall -Werror -c test/main.c -o build/test/main.o
--- test/input.c
+++ test/input.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int h, kol, i = 0;
+int h, kol, i = 0, bulls, cows;
 char *hchar = NULL;
 char *ichar = NULL;
 
@@ -100,4 +100,46 @@ CTEST(input, scan_number)
     ASSERT_EQUAL(exp6, c6);
     ASSERT_EQUAL(exp7, c7);
     ASSERT_EQUAL(exp8, c8);
+}
+
+CTEST(counter, bulls_cows) {
+
+    int c1 = how_much_bulls(01234, 98765, 5);
+    int c2 = how_much_cows(01234, 98765, 5);
+    int c3 = how_much_bulls(9876, 5436, 4);
+    int c4 = how_much_cows(9876, 5436, 4);
+    int c5 = how_much_bulls(129083, 129083, 6);
+    int c6 = how_much_cows(129083, 129083, 6);
+    int c7 = how_much_bulls(129083, 129803, 6);
+    int c8 = how_much_cows(129083, 129803, 6);
+    int c9 = how_much_bulls(1290, 1209, 4);
+    int c10 = how_much_cows(1290, 1209, 4);
+    int c11 = how_much_bulls(12908, 12980, 5);
+    int c12 = how_much_cows(12908, 12980, 5);
+
+    const int exp1 = 0;
+    const int exp2 = 0;
+    const int exp3 = 1;
+    const int exp4 = 0;
+    const int exp5 = 6;
+    const int exp6 = 0;
+    const int exp7 = 4;
+    const int exp8 = 2;
+    const int exp9 = 2;
+    const int exp10 = 3;
+    const int exp11 = 2;
+    const int exp12 = 2;
+
+    ASSERT_EQUAL(exp1, c1);
+    ASSERT_EQUAL(exp2, c2);
+    ASSERT_EQUAL(exp3, c3);
+    ASSERT_EQUAL(exp4, c4);
+    ASSERT_EQUAL(exp5, c5);
+    ASSERT_EQUAL(exp6, c6);
+    ASSERT_EQUAL(exp7, c7);
+    ASSERT_EQUAL(exp8, c8);
+    ASSERT_EQUAL(exp9, c9);
+    ASSERT_EQUAL(exp10, c10);
+    ASSERT_EQUAL(exp11, c11);
+    ASSERT_EQUAL(exp12, c12);
 }
